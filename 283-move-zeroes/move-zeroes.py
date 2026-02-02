2082497e7090948f6af38b765266b6b88f118d09
Joshua Jenisus R <jenisusjr07@gmail.com>
2026-02-02 12:30:52 +0530

    Time: 3 ms (81.93%) | Memory: 20.7 MB (13.06%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,7 +1,11 @@
-class Solution:
-    def moveZeroes(self, nums: list[int]) -> None:
-        write = 0
+class Solution(object):
+    def moveZeroes(self, nums):
+
+        non_zero = 0  # Pointer for non-zero elements
+        
+        # Move all non-zero elements to the front
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[write], nums[i] = nums[i], nums[write]
-                write += 1  
+                nums[i], nums[non_zero] = nums[non_zero], nums[i]
+                non_zero += 1
+        
