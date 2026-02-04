4799b621b163b49d4d9b3d805ae16a44c00f8411
Joshua Jenisus R <jenisusjr07@gmail.com>
2026-02-04 12:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (84.30%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -5,7 +5,7 @@ class Solution {
         if (nums[low] < nums[high]) return nums[low];
         
         while (low < high) {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
             
             if (nums[mid] > nums[high]) {
                 low = mid + 1;
