48c18150a46f71769b7cb26d2cb2b91e757881a8
Joshua Jenisus R <jenisusjr07@gmail.com>
2026-02-05 13:56:53 +0530

    Time: 30 ms (70.40%) | Memory: 147.1 MB (41.92%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -4,13 +4,13 @@ class Solution {
         int[] result = new int[n - k + 1];        
         Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k ) {
                 deque.pollFirst();
             }            
             while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
                 deque.pollLast();
             }
-            deque.offerLast(i);            
+            deque.addLast(i);            
             if (i >= k - 1) {
                 result[i - k + 1] = nums[deque.peekFirst()];
             }
